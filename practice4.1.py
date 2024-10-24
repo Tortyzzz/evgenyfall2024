3ef6031ffbf0df2de34812ea0dac0192a701abf4
evgeny emelyanov <evgen10.emelyanov@gmail.com>
2024-10-24 22:48:41 +0700

    Delete practice4.1.py

--- practice4.1.py
+++
@@ -1,7 +0,0 @@
-def tr(metres):
-    cent = metres * 100
-    return cent
-print('Enter a value in metres: ')
-metres = int(input())
-cent = tr(metres)
-print('Distance in metres: ', metres, ' and centimetres', cent)
