f8bcf4ef032efc10a4e9c3add176e942655f1677
evgeny emelyanov <evgen10.emelyanov@gmail.com>
2024-10-24 22:49:53 +0700

    Delete practice4.3.py

--- practice4.3.py
+++
@@ -1,9 +0,0 @@
-numbers = list(range(10))
-
-user_input = int(input("Введите число для вывода таблицы умножения: "))
-
-print("  ".join(str(num) for num in numbers))
-
-for number in numbers:
-    result = user_input * number
-    print(result, end=" ")
