e0ced43a179fcdb22b8125d3561ce96c6be94817
evgeny emelyanov <evgen10.emelyanov@gmail.com>
2024-10-24 22:49:08 +0700

    Delete practice4.10.py

--- practice4.10.py
+++
@@ -1,7 +0,0 @@
-a = int(input('Enter a '))
-b = int(input('Enter b '))
-print('Введенные значения а = ', a, 'и значение b = ', b)
-c = a
-a = b
-b = c
-print('Введенные значения а = ', a, 'и значение b = ', b)
